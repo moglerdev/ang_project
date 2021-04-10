5b176a02eb615a511f8d3f28784139e2b32b626b
Christopher Mogler <christopher@mogler.dev>
2021-04-11 01:23:22 +0200

    Button Klasse fertiggestellt

--- source/button.cpp
+++ source/button.cpp
@@ -1,6 +1,52 @@
 #include "button.h"
 
-Button::Button(QObject *parent) : QObject(parent)
+Button::Button(const QPoint& size, const QString& textContent, const QColor& textColor, const QBrush& background)
 {
+	QRect rect = QRect(0, 0, size.x(), size.y());
 
+	backgroundItem = new QGraphicsRectItem(rect);
+	textItem = new QGraphicsTextItem(textContent);
+
+	addToGroup(backgroundItem);
+	addToGroup(textItem);
+
+	QFont font = QFont("Arial", 18);
+	textItem->setFont(font);
+	
+	setTextPos(size);
+
+	backgroundItem->setBrush(background);
+	textItem->setDefaultTextColor(textColor);
+}
+
+void Button::setText(const QString& text, const QColor& color) {
+	textItem->setPlainText(text);
+	textItem->setDefaultTextColor(color);
+}
+
+void Button::setBackground(const QBrush& background) {
+	backgroundItem->setBrush(background);
+}
+
+void Button::setSize(const QPoint& size) {
+	backgroundItem->setRect(0, 0, size.x(), size.y());
+	setTextPos(size);
+}
+
+void Button::setTextPos(const QPoint& newSize) {
+	int t_x = (newSize.x() / 2) - textItem->boundingRect().width() / 2;
+	int t_y = (newSize.y() / 2) - textItem->boundingRect().height() / 2;
+	textItem->setPos(t_x, t_y);
+}
+
+void Button::mousePressEvent(QGraphicsSceneMouseEvent* eve) {
+	emit btnClicked(eve);
+}
+
+void Button::hoverEnterEvent(QGraphicsSceneHoverEvent* eve) {
+	emit mouseEnter(eve);
+}
+
+void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent* eve) {
+	emit mouseLeave(eve);
 }
--- source/button.h
+++ source/button.h
@@ -2,15 +2,39 @@
 #define BUTTON_H
 
 #include <QObject>
+#include <QBrush>
+#include <QGraphicsScene>
+#include <QGraphicsItemGroup>
+#include <QPropertyAnimation>
+#include <QGraphicsPixmapItem>
 
-class Button : public QObject
+class Button : public QObject, public QGraphicsItemGroup
 {
     Q_OBJECT
 public:
-    explicit Button(QObject *parent = nullptr);
+    explicit Button(const QPoint& size, const QString& textContent, const QColor& textColor = Qt::black, const QBrush& background = QBrush(Qt::gray));
+
+    void setText(const QString& text, const QColor& color);
+
+    void setBackground(const QBrush& background);
+
+    void setSize(const QPoint& size);
 
 signals:
+    void btnClicked(QGraphicsSceneMouseEvent* eve);
+    void mouseEnter(QGraphicsSceneHoverEvent* eve);
+    void mouseLeave(QGraphicsSceneHoverEvent* eve);
 
+private:
+    QGraphicsRectItem* backgroundItem;
+    QGraphicsTextItem* textItem;
+
+    void setTextPos(const QPoint& newSize);
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent* eve);
+    void hoverEnterEvent(QGraphicsSceneHoverEvent* eve);
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent* eve);
 };
 
 #endif // BUTTON_H
